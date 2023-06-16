bfeded0a7458faddf7c91b87a0dc7e5602d0d1e1
Jeremy Pratt <jerpratt1@gmail.com>
2023-06-16 19:53:03 -0400

    Made List Visible, minor CSS

--- recipe-vue/recipe-vue/src/components/AddRecipe.vue
+++ recipe-vue/recipe-vue/src/components/AddRecipe.vue
@@ -1,25 +1,137 @@
 <template>
   <div>
-    <form id='add_recipe' v-on:submit.prevent ="submitRecipe" >
-      <label for="title" id="title">Recipe Title: <br/><input type="text" name="title" class="text" v-model="recipeCard.recipeTitle"> </label>
-      <label for="description" id="description">Recipe Description: <br/><input type="text" name="description" class="text" v-model="recipeCard.recipeDescription"> </label>
-      <label for="prep" id="prep">Prep Time: <br/><input type="number" name="prep" v-model="recipeCard.prepTime"> </label>
-      <label for="cook" id="cook">Cook Time: <br/><input type="number" name="cook" v-model="recipeCard.cookTime"> </label>
-      <label for="rest" id="rest">Rest Time: <br/><input type="number" name="rest" v-model="recipeCard.restTime"></label>
-      <label for="serving" id="serving">Serving Size: <br/><input type="number" name="serving" v-model="recipeCard.servingSize"></label>
-      <label for="tools" id="tools">Tools: <br/><input type="text" name="tools" class="short_text" v-model="recipeCard.tools"> <button> Add </button></label>
-      <label for="ingredients" id="ingredients">Ingredients: <br/><input type="text" name="ingredients" class="text" v-model="recipeCard.ingredients"> <button> Add </button></label>
-      <label for="instructions" id="instructions">Instructions: <br/><input type="text" name="instructions" class="text tall" v-model="recipeCard.instructions"> <button> Add </button> </label>
-      <label for="notes" id="notes">Notes: <br/><input type="text" name="notes" class="text tall" v-model="recipeCard.notes"> <button> Add </button></label>
-      <label for="keywords" id="keywords">Keywords: <br/><input type="text" name="keywords" class="text" v-model="recipeCard.keyWords"> <button> Add </button> </label>
-      <button id="button"> Submit Recipe </button>
-    </form>
-    
+    <div id="add_recipe">
+      <label for="title" id="title"
+        >Recipe Title: <br /><input
+          type="text"
+          name="title"
+          class="text"
+          v-model="recipeCard.recipeTitle"
+        />
+      </label>
+      <label for="description" id="description"
+        >Recipe Description: <br /><input
+          type="text"
+          name="description"
+          class="text"
+          v-model="recipeCard.recipeDescription"
+        />
+      </label>
+      <label for="prep" id="prep"
+        >Prep Time: <br /><input
+          type="number"
+          name="prep"
+          v-model="recipeCard.prepTime"
+        />
+      </label>
+      <label for="cook" id="cook"
+        >Cook Time: <br /><input
+          type="number"
+          name="cook"
+          v-model="recipeCard.cookTime"
+        />
+      </label>
+      <label for="rest" id="rest"
+        >Rest Time: <br /><input
+          type="number"
+          name="rest"
+          v-model="recipeCard.restTime"
+      /></label>
+      <label for="serving" id="serving"
+        >Serving Size: <br /><input
+          type="number"
+          name="serving"
+          v-model="recipeCard.servingSize"
+      /></label>
+      <div id="tools">
+        <label for="tools"
+          >Tools: <br /><input
+            type="text"
+            name="tools"
+            class="short_text"
+            v-model="activeTool"
+          />
+          <button v-on:click="addToArray(activeTool, toolList)">
+            Add
+          </button></label
+        >
+        <div v-for="tool in toolList" v-bind:key="tool.id">{{ tool }}</div>
+      </div>
+      <div id="ingredients">
+        <label for="ingredients"
+          >Ingredients: <br /><input type="number" />
+          <select name="subject" id="subject">
+            <option value="" selected="selected">OZ</option>
+            <option value="" selected="selected">TSP</option></select
+          ><input
+            type="text"
+            name="ingredients"
+            class="text"
+            v-model="activeIngredient"
+          />
+          <button v-on:click="addToArray(activeIngredient, ingredientList)">
+            Add
+          </button></label
+        >
+        <div v-for="ingredient in ingredientList" v-bind:key="ingredient.id">
+          {{ ingredient }}
+        </div>
+      </div>
+      <div id="instructions">
+        <label for="instructions"
+          >Instructions: <br /><input
+            type="text"
+            name="instructions"
+            class="text tall"
+            v-model="activeInstruction"
+          />
+          <button v-on:click="addToArray(activeInstruction, instructionList)">
+            Add
+          </button>
+        </label>
+        <div v-for="instruction in instructionList" v-bind:key="instruction.id">
+          {{ instruction }}
+        </div>
+      </div>
+      <div id="notes">
+        <label for="notes"
+          >Notes: <br /><input
+            type="text"
+            name="notes"
+            class="text tall"
+            v-model="activeNote"
+          />
+          <button v-on:click="addToArray(activeNote, noteList)">
+            Add
+          </button></label
+        >
+        <div v-for="note in noteList" v-bind:key="note.id">{{ note }}</div>
+      </div>
+      <div id="keywords">
+        <label for="keywords"
+          >Keywords: <br /><input
+            type="text"
+            name="keywords"
+            class="text"
+            v-model="activeKeyWord"
+          />
+          <button v-on:click="addToArray(activeKeyWord, keyWordList)">
+            Add
+          </button>
+        </label>
+        <div v-for="keyWord in keyWordList" v-bind:key="keyWord.id">
+          {{ keyWord }}
+        </div>
+      </div>
+      <button id="button" v-on:submit.prevent="submitRecipe">
+        Submit Recipe
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import RecipeService from '../services/RecipeService.js'
+import RecipeService from "../services/RecipeService.js";
 export default {
   data() {
     return {
@@ -36,117 +148,127 @@ export default {
         notes: "",
         keyWords: "",
       },
-    }
+      activeTool: "",
+      activeIngredient: "",
+      activeInstruction: "",
+      activeNote: "",
+      activeKeyWord: "",
+      toolList: [],
+      ingredientList: [],
+      instructionList: [],
+      noteList: [],
+      keyWordList: [],
+    };
   },
   methods: {
     submitRecipe() {
-        RecipeService.addRecipe(this.recipeCard).then((response) =>{
-            console.log(response.status);
-
+      RecipeService.addRecipe(this.recipeCard)
+        .then((response) => {
+          console.log(response.status);
         })
         .catch((error) => {
           console.log(error.response.status);
-        })
-    }
-  }
+        });
+    },
 
-}
+    addToArray(activeForm, arrayName) {
+      arrayName.push(activeForm);
+    },
+  },
+};
 </script>
 
 <style scoped>
-#title{
+#title {
   grid-area: title;
-  width: 100%
+  width: 100%;
 }
 
-#description{
+#description {
   grid-area: description;
-  width: 100%
+  width: 100%;
 }
 
-#prep{
+#prep {
   grid-area: prep;
-  width: 100%
+  width: 100%;
 }
 
-#cook{
+#cook {
   grid-area: cook;
-  width: 100%
+  width: 100%;
 }
 
-#rest{
+#rest {
   grid-area: rest;
-  width: 100%
+  width: 100%;
 }
 
-#serving{
+#serving {
   grid-area: serving;
-  width: 100%
+  width: 100%;
 }
 
-#tools{
+#tools {
   grid-area: tools;
-  width: 100%
+  width: 100%;
 }
 
-#ingredients{
+#ingredients {
   grid-area: ingredients;
-  width: 100%
+  width: 100%;
 }
 
-#instructions{
+#instructions {
   grid-area: instructions;
-  width: 100%
+  width: 100%;
 }
 
-#notes{
+#notes {
   grid-area: notes;
-  width: 100%
+  width: 100%;
 }
 
-#keywords{
+#keywords {
   grid-area: keywords;
-  width: 100%
+  width: 100%;
 }
 
-#button{
+#button {
   grid-area: button;
   width: 33%;
   justify-self: center;
 }
 
-
-
-#add_recipe{
-display: grid;
-grid-template-columns: 3fr 3fr 3fr;
-grid-template-rows: auto;
-grid-template-areas:
- "title title x"
- "description description x"
- "prep cook rest"
- "serving tools tools"
- "ingredients ingredients ingredients"
- "instructions instructions instructions"
- "notes notes notes"
- "keywords keywords keywords"
- "button button button"
-;
-background: #282c34;
-height: 100vh;
-max-width: 100%;
-color: white;
+#add_recipe {
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "title title x"
+    "description description x"
+    "prep cook rest"
+    "serving tools tools"
+    "ingredients ingredients ingredients"
+    "instructions instructions instructions"
+    "notes notes notes"
+    "keywords keywords keywords"
+    "button button button";
+  background: #282c34;
+  height: 100vh;
+  max-width: 100%;
+  color: white;
 }
 
-.text{
+.text {
   width: 80%;
 }
 
-.short_text{
+.short_text {
   width: 70%;
 }
 
-.tall{
+.tall {
   height: 50px;
 }
 </style>
