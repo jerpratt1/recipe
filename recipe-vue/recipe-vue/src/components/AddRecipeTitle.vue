035d2b60bef02e605a33099375e36fb09a41f465
Jeremy Pratt <jerpratt1@gmail.com>
2023-06-22 14:23:14 -0400

    changing format

---
+++ recipe-vue/recipe-vue/src/components/AddRecipeTitle.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label for="title" id="title">
+        Recipe Title: <br /><input type="text" name="title" class="text" v-model="this.recipeTitle"/>
+    </label>
+    <label for="description" id="description">
+        Recipe Description: <br /><input type="text" name="description" class="text" v-model="this.recipeDescription"/>
+    </label>
+    <div id="keywords">
+        <label for="keywords">
+          Keywords: <br /><input type="text" name="keywords" class="text" v-model="activeKeyWord"/>
+          <button v-on:click="addToArray(activeKeyWord, keyWords)">
+            Add
+          </button>
+        </label>
+        <div v-for="keyWord in keyWords" v-bind:key="keyWord.id">
+          {{ keyWord }}
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        recipeTitle: "",
+        recipeDescription: "",
+        activeKeyWord: "",
+        keyWords: []
+        }
+    },
+
+    methods: {
+        setTitleDescriptionKeyWords(){
+            this.$store.SET_TITLE(this.recipeTitle);
+            this.$store.SET_DESCRIPTION(this.recipeTitle);
+            this.$store.SET_KEYWORDS(this.keyWords);
+
+        },
+
+        addToArray(activeForm, arrayName) {
+            arrayName.push(activeForm);
+        },
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- recipe-vue/recipe-vue/src/store/index.js
+++ recipe-vue/recipe-vue/src/store/index.js
@@ -6,7 +6,17 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     recipeToSubmit: {
-      
+        recipeTitle: "",
+        recipeDescription: "",
+        prepTime: 0,
+        cookTime: 0,
+        restTime: 0,
+        servingSize: 0,
+        tools: "",
+        ingredients: "",
+        instructions: "",
+        notes: "",
+        keyWords: "",
     }
 
 
@@ -15,6 +25,19 @@ export default new Vuex.Store({
   getters: {
   },
   mutations: {
+    SET_TITLE(title){
+      this.state.recipeToSubmit.recipeTitle = title;
+    },
+
+    SET_DESCRIPTION(description){
+      this.state.recipeToSubmit.recipeDescription = description;
+    },
+
+    SET_KEYWORDS(keyWords){
+      this.state.recipeToSubmit.keyWords = keyWords;
+    },
+
+
   },
   actions: {
   },
