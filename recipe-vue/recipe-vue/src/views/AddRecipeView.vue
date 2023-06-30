fd1150db9ff94d8ec899816305c7eedaa3153a2b
Jeremy Pratt <jerpratt1@gmail.com>
2023-06-30 18:05:03 -0400

    misc changes

--- recipe-vue/recipe-vue/src/views/AddRecipeView.vue
+++ recipe-vue/recipe-vue/src/views/AddRecipeView.vue
@@ -2,14 +2,16 @@
   <div>
     <add-recipe-title />
     <add-recipe-info />
+    <add-recipe />
   </div>
 </template>
 
 <script>
 import AddRecipeTitle from '@/components/AddRecipeTitle.vue'
 import AddRecipeInfo from '@/components/AddRecipeInfo.vue'
+import AddRecipe from '@/components/AddRecipe.vue'
 export default {
-    components: { AddRecipeTitle, AddRecipeInfo},
+    components: { AddRecipeTitle, AddRecipeInfo, AddRecipe},
 
 }
 </script>
